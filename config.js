5df69c6f1a22492a239024fee9b26e68bc88801a
LakshanRaghav <88176962+LakshanRaghav@users.noreply.github.com>
2026-04-24 03:37:05 +0530

    Remove hardcoded NVIDIA API key from config

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CONFIG = {
-    NVIDIA_API_KEY: process.env.NVIDIA_API_KEY || "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"
+    NVIDIA_API_KEY: process.env.NVIDIA_API_KEY
 };
